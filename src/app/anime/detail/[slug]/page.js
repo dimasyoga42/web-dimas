4c3868315ea6b947b7e921f02603b5fd940dc317
dimasyoga42 <dimasyoga9090@gmail.com>
2026-08-15 11:48:58 +0700

    penambahan search anime

--- src/app/anime/detail/[slug]/page.js
+++ src/app/anime/detail/[slug]/page.js
@@ -127,6 +127,8 @@ const AnimeDetailPage = async ({ params }) => {
                 Tonton Episode Terbaru
               </Link>
             )}
+            <Link href="/anime" className="inline-flex items-center gap-2 w-fit mt-3 border-2 border-black bg-green-500 px-6 py-2.5 font-heading text-sm font-extrabold uppercase text-white shadow-[4px_4px_0_0_#000] hover:bg-black hover:shadow-[6px_6px_0_0_#000] hover:-translate-x-0.5 hover:-translate-y-0.5 transition-all duration-150"
+          >Back Home</Link>
           </div>
         </div>
 
